27fa16f20c9a638850300e27dac1d9c45f4545be
SuperGoldenMan <seananderson10@hotmail.co.uk>
2025-03-10 17:44:25 +0000

    fixed some hover issues and added gradient radius effect

--- scripts/skills.js
+++ scripts/skills.js
@@ -10,3 +10,19 @@ document.querySelectorAll('.card').forEach(card => {
         document.getElementById(skill).classList.remove('hidden');
     });
 });
+
+
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+    
+    const rect = target.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+    
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  }
+  
+  for(const card of document.querySelectorAll(".card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+  }
