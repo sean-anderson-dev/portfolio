839b964208a91d9f1ee4f14b280fa6648078b351
sean-anderson-dev <seananderson10@hotmail.co.uk>
2025-10-10 22:30:16 +0100

    Added CP button to mobile and added active css/js functionality to mbs

--- scripts/mobile-nav.js
+++ scripts/mobile-nav.js
@@ -1,3 +1,4 @@
+// mobile menu toggle event
 const mobileMenuButton = document.getElementById("mobile-menu-button");
     const mobileMenu = document.getElementById("mobile-menu");
     const closeMenuButton = document.getElementById("close-menu");
@@ -16,3 +17,21 @@ const mobileMenuButton = document.getElementById("mobile-menu-button");
             mobileMenu.classList.add("hidden");
         }
     });
+
+// mobile menu selected section event
+const navButtons = document.querySelectorAll('.nav-button');
+
+function handleNavClick(event) {
+  //remove 'active' class from all buttons
+  navButtons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  //add 'active' class to the clicked button
+  event.currentTarget.classList.add('active');
+}
+
+//add a click event listener to each button
+navButtons.forEach(button => {
+  button.addEventListener('click', handleNavClick);
+});
